0b8fad80e1476be1de6d1b7dd15a10f6eed1ff4d
David Devoogdt <david.devoogdt@gmail.com>
2022-08-12 09:19:35 +0200

    jax tf_call: tensorflow batching

--- IMLCV/test/tf2jax.py
+++ IMLCV/test/tf2jax.py
@@ -4,10 +4,13 @@ from typing import Iterable
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jax import grad, jacfwd, jacrev, jit, vmap
+import tensorflow as tf
+from click import argument
+from jax import grad, jacrev, jit, make_jaxpr, vmap
 from jax.experimental import jax2tf
 from jax.experimental.jax2tf import call_tf, call_tf_p
 from jax.interpreters import batching
+from sympy import dict_merge
 
 
 def test_call_tf_batcher():
@@ -16,10 +19,14 @@ def test_call_tf_batcher():
     def f(x):
         return jnp.array([jnp.sum(x * x),  jnp.product(x)])
 
-    f_t = jax2tf.call_tf(jax2tf.convert(f, with_gradient=True))
-
     x = np.random.random((7))
     y = np.random.random((5, 7))
+
+    # add batcher for call_tf_p
+    batching.primitive_batchers[call_tf_p] = functools.partial(
+        loop_batcher, call_tf_p)
+
+    f_t = jax2tf.call_tf(jax2tf.convert(f, with_gradient=True))
 
     print(f"||f(x) - f_t(x)||_2 = { jnp.linalg.norm( f(x)-f_t(x) )  }")
 
@@ -31,59 +38,53 @@ def test_call_tf_batcher():
 
     j = jit(jacrev(f))
     j_t = jit(jacrev(f_t))
-    j_t = jit(jacrev(f_t))
 
     print(
         f"||jac(f)(x) - jac(f_t)(x)||_2 = { jnp.linalg.norm( j(x)-j_t(x) )  }")
 
-#new batcher
+
 def loop_batcher(prim, args, dims,  **params):
 
-    # determine new axis size
-    for ni, di in enumerate(dims):
-        if di is not None:
-            axis_size = args[ni].shape[di]
-            break
+    # do partial application of args on given position"
+    def apply(batch_args, f, static_args, static_pos):
+        arguments = []
 
-    # generate combination of indices for different arguments
-    args_indices = []
-    for xi, ia in zip(args, dims):
+        l = len(batch_args)+len(static_args)
+        j = 0
+        k = 0
+        for i in range(l):
+            if i in static_pos:
+                arguments.append(static_args[j])
+                j += 1
+            else:
+                arguments.append(batch_args[k])
+                k += 1
 
-        xs = xi.shape
-        indices = [Ellipsis if i !=
-                   ia else None for i, _ in enumerate(xs)]
+        return f(*arguments)
 
-        l = []
-        for i in range(axis_size):
-            if ia is not None:
-                indices[ia] = i
+    static_pos = []
+    static_args = []
+    batch_args = []
 
-            l.append(tuple(indices))
-        args_indices.append(l)
+    # find arguments for partial application
+    for i, (arg, batch_axis) in enumerate(zip(args, dims)):
+        if batch_axis is None:
+            static_pos.append(i)
+            static_args.append(arg)
+        else:
+            assert batch_axis == 0, 'other position not yet implemented'
+            batch_args.append(arg)
 
-    # apply function
-    out = []
-    for inds in list(zip(*args_indices),):
-        outp = prim.bind(*[a[b] for a, b in zip(
-            args, inds)], **params)
-        if not isinstance(outp, Iterable):
-            outp = tuple((outp,))
-        out.append(outp)
+    # vectorize
+    def par_fun(batch_args, static_args):
+        return tf.vectorized_map(fn=functools.partial(apply, f=params['callable_flat_tf'], static_args=static_args, static_pos=static_pos), elems=batch_args)
 
-    ret = []
+    if len(batch_args) != 1:
+        raise NotImplementedError
 
-    # collect output in arrays
-    for out_args in list(zip(*out)):
-        val = jax.numpy.hstack(out_args)
-        val = jax.numpy.reshape(val,  (axis_size,  *out_args[0].shape))
-
-        ret.append(val)
-
+    # execute with given arguments
+    ret = call_tf(par_fun)(batch_args, static_args)
     return (ret,  (0,)*len(ret))
-
-#add batcher for call_tf_p
-batching.primitive_batchers[call_tf_p] = functools.partial(
-    loop_batcher, call_tf_p)
 
 
 if __name__ == "__main__":
