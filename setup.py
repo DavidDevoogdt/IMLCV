ac45d6cca78688c11d99e518059ccd682f4e4776
David Devoogdt <david.devoogdt@ugent.be>
2023-05-09 16:02:04 +0200

    github actions from PyScaffold

--- setup.py
+++ setup.py
@@ -12,10 +12,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(
-            use_scm_version={"version_scheme": "no-guess-dev"},
-            # setup_requires=["Cython", " numpy"],
-        )
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
