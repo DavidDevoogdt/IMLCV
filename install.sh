6297e74b5d63571c51894f19a9a05020f752848d
David Devoogdt <david.devoogdt@ugent.be>
2023-05-10 11:19:23 +0200

    install deps

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 
 
 curl micro.mamba.pm/install.sh | bash
-micromamba -n .venv python=3.10
+micromamba -n .venv  install
 eval "$(micromamba shell hook --shell=bash)"
 micromamba activate .venv
-pip install tox setuptools setuptools_scm wheel
-pip  install -e .
+micromamba  install -y  python=3.10
+pip install tox setuptools setuptools_scm wheel Cython Numpy
+
+
+pip install .
