51743a84159e300c42c9d0dd1247ac12edcef11c
David Devoogdt <david.devoogdt@ugent.be>
2023-05-10 14:24:21 +0200

    python require 3.10

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 
 
 curl micro.mamba.pm/install.sh | bash
-micromamba -n .venv  install
+micromamba create -n .venv
 eval "$(micromamba shell hook --shell=bash)"
 micromamba activate .venv
-micromamba  install -y  python=3.10
-pip install tox setuptools setuptools_scm wheel Cython Numpy
-
-
-pip install .
+micromamba  install -y  python=3.10 tox
